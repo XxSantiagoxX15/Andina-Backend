74c50acadde1e1fea2aed8fc53994b27772a26c7
XxSantiagoxX15 <santiagomelo15@hotmail.com>
2025-03-25 13:04:49 -0500

    First Push

---
+++ src/main/java/co/edu/unbosque/andina/AndinaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.andina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndinaApplication.class, args);
+	}
+
+}
